4f67a909c6a232b07e1c6cedfe0ffdc9e7619f49
yourame404 <157692864+yourame404@users.noreply.github.com>
2025-06-05 11:28:04 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -68,7 +68,7 @@ resource "aws_route_table" "demo_rt" {
 resource "aws_subnet" "demo_subnet" {
   vpc_id                  = aws_vpc.demo_vpc.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-1"
+  availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
 
   tags = {
